f45c0a7114147d7aeacacd801f73678a77bcee3a
Danilo Diniz <danilodiniz93@yahoo.com>
2024-09-23 22:14:01 -0300

    Sports Rental application initialized

---
+++ src/main/java/tech/sportsrental/api/SportrentApplication.java
@@ -0,0 +1,13 @@
+package tech.sportsrental.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportrentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportrentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/sportsrental/api/SportrentApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.sportsrental.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportrentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
